0f87ce9c36259d1cc04d2730fc47f5116963d085
H0wZy <167232856+H0wZy@users.noreply.github.com>
2025-10-12 10:40:42 -0300

    feat: swagger to debug

--- backend/Auth.Api/Program.cs
+++ backend/Auth.Api/Program.cs
@@ -6,9 +6,11 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<AuthDbContext>(options =>
-    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"), 
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"),
         b => b.MigrationsHistoryTable("__EFMigrationsHistory", "auth")));
 
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
@@ -18,11 +20,19 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Auth.Api v1");
+        c.RoutePrefix = string.Empty;
+    });
 }
 
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+
+app.UseAuthentication();
 
 app.MapControllers();
 
