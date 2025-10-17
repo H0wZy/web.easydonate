759f9201ae69e6fe997ab7a3a741c99b5d9b0488
H0wZy <167232856+H0wZy@users.noreply.github.com>
2025-10-17 20:51:51 -0300

    feat: AutoMapper

---
+++ backend/User.Api/Mappings/UserProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using User.Api.Dto;
+using User.Api.Models;
+
+namespace User.Api.Mappings;
+
+public class UserProfile : Profile
+{
+    public UserProfile()
+    {
+        CreateMap<UpdateUserDto, UserModel>()
+            .ForAllMembers(opt
+                => opt.Condition((src, dest, srcMember)
+                    => srcMember is not null));
+    }
+}
