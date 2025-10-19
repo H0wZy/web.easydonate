f67ea0e41987d99c770c04efeb61c52cec2e397a
H0wZy <167232856+H0wZy@users.noreply.github.com>
2025-10-19 16:12:53 -0300

    feat: add JwtHelper class with method to generate JWT tokens

---
+++ backend/src/Auth.Api/Utils/JwtHelper.cs
@@ -0,0 +1,11 @@
+﻿using Auth.Api.Model;
+
+namespace Auth.Api.Utils;
+
+public class JwtHelper
+{
+    public static string GenerateJwt(AuthModel user, IConfiguration configuration)
+    {
+        return null;
+    }
+}
