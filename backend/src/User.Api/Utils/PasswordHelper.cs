b7d719a55f3809d17d3b4f70a36072860e74ceec
H0wZy <167232856+H0wZy@users.noreply.github.com>
2025-10-19 16:19:32 -0300

    feat: add PasswordHelper class for password hashing and verification

---
+++ backend/src/User.Api/Utils/PasswordHelper.cs
@@ -0,0 +1,21 @@
+﻿using System.Security.Cryptography;
+
+namespace User.Api.Utils;
+
+public static class PasswordHelper
+{
+    public static (byte[] hash, byte[] salt) HashPassword(string password)
+    {
+        // Gerar um salt aleatório
+        var salt = new byte[16];
+        RandomNumberGenerator.Fill(salt);
+        var hash = Rfc2898DeriveBytes.Pbkdf2(password, salt, 10000, HashAlgorithmName.SHA256, 32);
+        return (hash, salt);
+    }
+
+    public static bool VerifyPassword(string password, byte[] savedHash, byte[] savedSalt)
+    {
+        var verifyHash = Rfc2898DeriveBytes.Pbkdf2(password, savedSalt, 10000, HashAlgorithmName.SHA256, 32);
+        return verifyHash.SequenceEqual(savedHash);
+    }
+}
