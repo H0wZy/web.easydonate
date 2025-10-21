5aa5949a6cdeb605f4e78bac5700a466aae2354e
H0wZy <167232856+H0wZy@users.noreply.github.com>
2025-10-21 20:47:45 -0300

    feat: new test endpoint

--- backend/src/Auth.Api/Controllers/AuthController.cs
+++ backend/src/Auth.Api/Controllers/AuthController.cs
@@ -1,17 +1,27 @@
 using Auth.Api.Dto;
+using Auth.Api.Model;
+using Auth.Api.Services.AuthService;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Auth.Api.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class AuthController : ControllerBase
+    public class AuthController(IAuthService authService) : ControllerBase
     {
         [HttpPost("login")]
         public async Task<IActionResult> Login([FromBody] LoginDto loginDto)
         {
             // A lógica de autenticação virá aqui
             return Ok("Login recebido com sucesso.");
+        }
+
+        [HttpGet("test-user/{email}")]
+        public async Task<ActionResult<ResponseModel<UserDto>>> AuthenticateAsync(string email, string username, string password)
+        {
+            var loginDto = new LoginDto ("username", email, "password");
+            var response = await authService.AuthenticateAsync(loginDto);
+            return response.Success ? Ok(response) : BadRequest(response);
         }
     }
 }
