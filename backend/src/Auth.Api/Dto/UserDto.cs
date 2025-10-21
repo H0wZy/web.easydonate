68b14e3466a0a30305ca49b1e80c351a16e55a55
H0wZy <167232856+H0wZy@users.noreply.github.com>
2025-10-20 23:45:27 -0300

    feat: add UserDto to define user data structure in Auth.Api

---
+++ backend/src/Auth.Api/Dto/UserDto.cs
@@ -0,0 +1,17 @@
+﻿namespace Auth.Api.Dto
+{
+    public class UserDto
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string HashPassword { get; set; }
+        public string SaltPassword { get; set; }
+        public int UserType { get; set; }
+        public bool IsUserDisabled { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
