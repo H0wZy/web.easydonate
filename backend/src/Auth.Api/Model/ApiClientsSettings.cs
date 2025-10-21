2bad6274ad6e7dea0f6f9433dc69d16e0494ea9c
H0wZy <167232856+H0wZy@users.noreply.github.com>
2025-10-20 23:40:54 -0300

    feat: add ApiClientsSettings model to encapsulate API client configuration

---
+++ backend/src/Auth.Api/Model/ApiClientsSettings.cs
@@ -0,0 +1,12 @@
+﻿namespace Auth.Api.Model;
+
+public class ApiClientsSettings(UserApiSettings userApi)
+{
+    public UserApiSettings UserApi { get; init; } = userApi;
+}
+
+public class UserApiSettings(string baseUrl)
+{
+    public string BaseUrl { get; init; } = baseUrl;
+    public int Timeout { get; init; }
+}
