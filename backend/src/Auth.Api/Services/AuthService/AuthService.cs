4d816c252d40c4b6026e83b89b00e9b79223fc57
H0wZy <167232856+H0wZy@users.noreply.github.com>
2025-10-20 23:42:28 -0300

    feat: implement AuthenticateAsync method with IUserApiClient injection in AuthService

--- backend/src/Auth.Api/Services/AuthService/AuthService.cs
+++ backend/src/Auth.Api/Services/AuthService/AuthService.cs
@@ -1,6 +1,16 @@
-﻿namespace Auth.Api.Services.AuthService;
+﻿using Auth.Api.Clients;
+using Auth.Api.Dto;
+using Auth.Api.Model;
 
-public class AuthService
+namespace Auth.Api.Services.AuthService
 {
-    
+    public class AuthService(IUserApiClient userApiClient) : IAuthService
+    {
+        public async Task<ResponseModel<LoginResponseDto>> AuthenticateAsync(LoginDto dto)
+        {
+            var user = await userApiClient.GetUserByEmailAsync(dto.Email);
+            // A implementação virá aqui
+            throw new NotImplementedException();
+        }
+    }
 }
