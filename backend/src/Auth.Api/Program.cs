478b8d639cbd83a9a46b03110b246c30c5f9b378
H0wZy <167232856+H0wZy@users.noreply.github.com>
2025-10-20 23:44:55 -0300

    feat: register IUserApiClient and IAuthService in DI container, configure UserApiClient HttpClient with timeout settings

--- backend/src/Auth.Api/Program.cs
+++ backend/src/Auth.Api/Program.cs
@@ -1,23 +1,33 @@
+using Auth.Api.Clients;
+using Auth.Api.Model;
+using Auth.Api.Services.AuthService;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+builder.Services.Configure<ApiClientsSettings>(builder.Configuration.GetSection("ApiClients"));
+builder.Services.AddHttpClient<UserApiClient>(client =>
+{
+    var apiSettings = builder.Configuration.GetSection("ApiClients:UserApi").Get<UserApiSettings>();
+    if (apiSettings != null) client.Timeout = TimeSpan.FromSeconds(apiSettings.Timeout);
+});
+
+builder.Services.AddScoped<IAuthService, AuthService>();
+builder.Services.AddScoped<IUserApiClient, UserApiClient>();
+
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
