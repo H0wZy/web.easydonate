ade3571a705ae428446ee7ca4208fcf2d85615b2
H0wZy <167232856+H0wZy@users.noreply.github.com>
2025-10-21 20:45:18 -0300

    fix: constructor

--- backend/src/Auth.Api/Model/ApiClientsSettings.cs
+++ backend/src/Auth.Api/Model/ApiClientsSettings.cs
@@ -1,12 +1,15 @@
-﻿namespace Auth.Api.Model;
-
-public class ApiClientsSettings(UserApiSettings userApi)
+﻿namespace Auth.Api.Model
 {
-    public UserApiSettings UserApi { get; init; } = userApi;
-}
+    public class ApiClientsSettings
+    {
+        public ApiClientsSettings() { } // construtor padrão obrigatório
+        public UserApiSettings UserApi { get; set; } = new();
+    }
 
-public class UserApiSettings(string baseUrl)
-{
-    public string BaseUrl { get; init; } = baseUrl;
-    public int Timeout { get; init; }
+    public class UserApiSettings
+    {
+        public UserApiSettings() { } // construtor padrão obrigatório
+        public string BaseUrl { get; set; } = string.Empty;
+        public int Timeout { get; set; } = 30; // valor padrão opcional
+    }
 }
